0e411c86513606e96efa68e008ce978176561759
Everton dos Santos Silva <evertonsantosilva18@gmail.com>
2021-06-26 15:46:11 -0300

    Inclusao tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/* global module*/
+module.exports = {
+  prefix: '',
+  important: true,
+  purge: {
+    content: [
+      './src/**/*.{html, ts}',
+    ]
+  },
+  darkMode: 'class',
+  theme: {
+    minHeight: {
+      '0': '0',
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+      'full': '100%',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
