8635c4e47511dde456939772930a2822a4bb6203
Everton dos Santos Silva <evertonsantosilva18@gmail.com>
2021-06-28 01:41:41 -0300

    Adicao rota e component 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { HomePageComponent } from './home-page/home-page.component'
+import { NotFoundComponent } from './not-found/not-found.component'
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: HomePageComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   }
 ]
 
